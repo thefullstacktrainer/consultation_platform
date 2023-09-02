20b03c9c7988b2bf5c692c93dfb0f54c5de90f82
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-09-02 19:27:13 +0530

    changes

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
     return (
-        <div>Sidebar</div>
-    )
+        <div className="sidebar">
+            <Link to="/">Dashboard</Link>
+            <Link to="/my-profile">My Profile</Link>
+            {/* ... add other side nav links */}
+        </div>
+    );
 }
 
-export default Sidebar
+export default Sidebar;

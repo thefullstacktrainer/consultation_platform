b74f3a82f3caf1abc9f38e39ad32b380d313a5be
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-09-06 16:30:01 +0530

    modularization

--- frontend/src/components/consultations/Consultations.js
+++ frontend/src/components/consultations/Consultations.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import AddConsultant from './AddConsultant';
 import ShowConsultant from './ShowConsultant';
 import EditConsultant from './EditConsultant';
+import DeleteConsultant from './DeleteConsultant';
+import DisplayConsultants from './DisplayConsultants';
 
 const baseURL = "http://localhost:5000/api/v1/consultants";
 const Consultations = () => {
@@ -176,113 +178,35 @@ const Consultations = () => {
                     Add Consultation
                 </button>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {consultationsData.map((consultation, index) => (
-                    <div key={index} className="bg-white p-4 rounded-lg shadow-md flex">
-                        <div className="flex-grow">
-                            <h2 className="text-xl font-semibold mb-2">{consultation.topic}</h2>
-                            <p className="mb-2"><strong>Consultant:</strong> {consultation.consultant}</p>
-                            <p className="mb-2"><strong>Date:</strong> {consultation.date}</p>
-                            <p className="mb-2"><strong>Time:</strong> {consultation.time}</p>
-                            <p className="mb-2"><strong>Duration:</strong> {consultation.duration}</p>
-                            <p className="mb-2"><strong>Spots Left:</strong> {consultation.spotsLeft}</p>
-                            <button className="mt-2 bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600">
-                                Book Now
-                            </button>
-                        </div>
-                        <div className="relative flex items-start">
-                            <button className="text-gray-500" onClick={() => toggleDropdownMenu(consultation.id)}>
-                                ...
-                            </button>
-                            {toggleDropdown === consultation.id && (
-                                <div ref={dropdownRef} className="dropdown absolute right-0 bg-white border border-gray-200 rounded mt-2 shadow-lg">
-                                    <button
-                                        className="block px-4 py-2"
-                                        onClick={() => {
-                                            showConsultationDetails(consultation);
-                                            setToggleDropdown(false);
-                                        }}
-                                    >
-                                        Show
-                                    </button>
-
-                                    <button className="block px-4 py-2" onClick={() => {
-                                        openEditDialog(consultation);
-                                        setToggleDropdown(false);
-                                    }}
-                                    >Edit</button>
-
-                                    <button
-                                        className="block px-4 py-2"
-                                        onClick={() => {
-                                            setConsultationToDelete(consultation.id);
-                                            setIsConfirmDialogVisible(true);
-                                            setToggleDropdown(false);
-                                        }}
-                                    >
-                                        Delete
-                                    </button>
-
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                ))}
-            </div>
+            <DisplayConsultants consultationsData={consultationsData}
+                toggleDropdownMenu={toggleDropdownMenu}
+                showConsultationDetails={showConsultationDetails}
+                setToggleDropdown={setToggleDropdown}
+                openEditDialog={openEditDialog}
+                setConsultationToDelete={setConsultationToDelete}
+                setIsConfirmDialogVisible={setIsConfirmDialogVisible}
+                toggleDropdown={toggleDropdown}
+                dropdownRef={dropdownRef} />
 
             {showCreateDialog &&
-                <AddConsultant newConsultation={newConsultation} handleInputChange={handleInputChange}
-                    handleAddConsultation={handleAddConsultation} setShowCreateDialog={setShowCreateDialog} />}
+                <AddConsultant newConsultation={newConsultation}
+                    handleInputChange={handleInputChange}
+                    handleAddConsultation={handleAddConsultation}
+                    setShowCreateDialog={setShowCreateDialog} />}
 
-            {isConfirmDialogVisible && (
-                <div className="fixed z-10 inset-0 overflow-y-auto">
-                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                        <div className="fixed inset-0 transition-opacity">
-                            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-                        </div>
+            {isConfirmDialogVisible
+                && <DeleteConsultant confirmDeleteConsultation={confirmDeleteConsultation}
+                    setIsConfirmDialogVisible={setIsConfirmDialogVisible} />}
 
-                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+            {consultationToShow
+                && <ShowConsultant consultationToShow={consultationToShow}
+                    showConsultationDetails={showConsultationDetails} />}
 
-                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                <div>
-                                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                                        Delete Consultation
-                                    </h3>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Are you sure you want to delete this consultation? This action cannot be undone.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                <button
-                                    type="button"
-                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
-                                    onClick={confirmDeleteConsultation}
-                                >
-                                    Delete
-                                </button>
-                                <button
-                                    type="button"
-                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                    onClick={() => setIsConfirmDialogVisible(false)}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
-            {consultationToShow && <ShowConsultant consultationToShow={consultationToShow}
-                showConsultationDetails={showConsultationDetails} />}
-
-            {showEditDialog && <EditConsultant editConsultation={editConsultation}
-                handleInputChangeForEdit={handleInputChangeForEdit}
-                handleEditConsultation={handleEditConsultation}
-                setShowEditDialog={setShowEditDialog} />}
+            {showEditDialog
+                && <EditConsultant editConsultation={editConsultation}
+                    handleInputChangeForEdit={handleInputChangeForEdit}
+                    handleEditConsultation={handleEditConsultation}
+                    setShowEditDialog={setShowEditDialog} />}
         </div>
     );
 };
---
+++ frontend/src/components/consultations/DisplayConsultants.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+function DisplayConsultants({ consultationsData, toggleDropdownMenu, showConsultationDetails, setToggleDropdown,
+    openEditDialog, setConsultationToDelete, setIsConfirmDialogVisible, toggleDropdown, dropdownRef }) {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {consultationsData.map((consultation, index) => (
+                <div key={index} className="bg-white p-4 rounded-lg shadow-md flex">
+                    <div className="flex-grow">
+                        <h2 className="text-xl font-semibold mb-2">{consultation.topic}</h2>
+                        <p className="mb-2"><strong>Consultant:</strong> {consultation.consultant}</p>
+                        <p className="mb-2"><strong>Date:</strong> {consultation.date}</p>
+                        <p className="mb-2"><strong>Time:</strong> {consultation.time}</p>
+                        <p className="mb-2"><strong>Duration:</strong> {consultation.duration}</p>
+                        <p className="mb-2"><strong>Spots Left:</strong> {consultation.spotsLeft}</p>
+                        <button className="mt-2 bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600">
+                            Book Now
+                        </button>
+                    </div>
+                    <div className="relative flex items-start">
+                        <button className="text-gray-500" onClick={() => toggleDropdownMenu(consultation.id)}>
+                            ...
+                        </button>
+                        {toggleDropdown === consultation.id && (
+                            <div ref={dropdownRef} className="dropdown absolute right-0 bg-white border border-gray-200 rounded mt-2 shadow-lg">
+                                <button
+                                    className="block px-4 py-2"
+                                    onClick={() => {
+                                        showConsultationDetails(consultation);
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    Show
+                                </button>
+
+                                <button className="block px-4 py-2" onClick={() => {
+                                    openEditDialog(consultation);
+                                    setToggleDropdown(false);
+                                }}
+                                >Edit</button>
+
+                                <button
+                                    className="block px-4 py-2"
+                                    onClick={() => {
+                                        setConsultationToDelete(consultation.id);
+                                        setIsConfirmDialogVisible(true);
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    Delete
+                                </button>
+
+                            </div>
+                        )}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default DisplayConsultants

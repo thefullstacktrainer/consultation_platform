ab69a35c59fadbc7653be4a4175cadea4ae36f5f
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-09-07 13:35:08 +0530

    changes

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,6 +1,9 @@
 let users = require('../data/users.json')
 const filename = './data/users.json'
 const helper = require('../helpers/helper.js')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 function registerUser(newUserInfo) {
     return new Promise((resolve, reject) => {
@@ -16,19 +19,28 @@ function registerUser(newUserInfo) {
     })
 }
 
-
-
 function loginUser(userInfo) {
-    return new Promise((resolve, reject) => {
-        const id = { id: helper.getNewId(users) }
-        const date = {
-            createdAt: helper.newDate(),
-            updatedAt: helper.newDate()
+    return new Promise(async (resolve, reject) => {
+        const user = users.find(user => user.username === userInfo.username)
+        if (user == null) {
+            reject({ message: "user is not found" });
+        } else {
+            try {
+                console.log("wer are", userInfo.password, user.password)
+                bcrypt.compare(userInfo.password, user.password, function (err, result) {
+                    // 'result' will be true if the passwords match, false otherwise
+                    if (result) {
+                        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+                        console.log("test1")
+                        resolve({ accessToken });
+                    } else {
+                        reject({ message: 'Not Allowed' });
+                    }
+                });
+            } catch {
+                reject({ message: "Not allowed" });
+            }
         }
-        newUserInfo = { ...newUserInfo, ...date, ...id, }
-        users.push(newUserInfo)
-        helper.writeJSONFile(filename, users)
-        resolve(newPost)
     })
 }
 
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const users = require('../models/users.model')
 router.post('/register', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.username, password: hashedPassword };
+        const user = { username: req.body.username, password: hashedPassword };
         users.registerUser(user)
             .then(data => res.status(201).send())
             .catch(error => res.status(500).send())
@@ -16,21 +15,12 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name);
-    if (user == null) {
-        return res.status(400).send('Cannot find user');
-    }
+    users.loginUser(req.body)
+        .then(user => {
+            res.status(200).send({ user });
+        })
+        .catch(error => res.status(400).send({ error }))
 
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-            res.json({ accessToken });
-        } else {
-            res.send('Not Allowed');
-        }
-    } catch {
-        res.status(500).send();
-    }
 });
 
 module.exports = router
